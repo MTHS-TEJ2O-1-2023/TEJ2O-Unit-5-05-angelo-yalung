c5d1d9ad5c7ba7b481e3b1917444ceb8098071e1
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-11 13:24:54 -0400

    j77\

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Angelo Yalung
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
