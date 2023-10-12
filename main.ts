43ff67bfec3fca08944667d6aba23c892a32ce21
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-12 12:51:20 -0400

    bnm

--- main.ts
+++ main.ts
@@ -7,19 +7,30 @@
 
 // so i dont see a massive amount of errors
 let neopixelStrip: neopixel.Strip = null
-let myStrip: neopixel.Strip = null
+
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.show()
-  basic.pause(5000)
+  basic.pause(3000)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
   neopixelStrip.show()
-  basic.pause(5000)
+  basic.pause(3000)
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
   neopixelStrip.show()
-  basic.pause(5000)
+  basic.pause(3000)
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
 })
