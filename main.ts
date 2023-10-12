df3883392669fc0fe5c05aefe14efa945dae51ed
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-12 09:35:36 -0400

    giggle water

--- main.ts
+++ main.ts
@@ -4,3 +4,21 @@
  * Created on: Oct 2023
  * This program turns the microbit into a traffic light
 */
+
+// so i dont see a massive amount of errors
+let neopixelStrip: neopixel.Strip = null
+
+input.onButtonPressed(Button.A, function () {
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.show()
+  basic.pause(5000)
+
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.show()
+  basic.pause(5000)
+
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.show()
+  basic.pause(5000)
+})
