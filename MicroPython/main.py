c8232d9c7165bf8127009cc8f69bb77dc61ce12d
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-12 17:29:33 +0000

    df

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,26 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Angelo Yalung
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        np[0] = (0, 255, 0)
+        np.show()
+        sleep(5000)
+        clear()
+
+        np[1] = (255, 255, 0)
+        np.show()
+        sleep(5000)
+        clear()
+
+        np[2] = (255, 0, 0)
+        np.show()
+        sleep(5000)
+        clear()
